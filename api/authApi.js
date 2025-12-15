1004262343a1b19eeef1caf8d8638ee8e962d301
Vulcherashish <iambestgod4@gmail.com>
2025-12-15 06:38:55 -0500

    Add files via upload

---
+++ api/authApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API = "http://localhost:5000/api/auth";
+
+export const loginUser = (data) => axios.post(`${API}/login`, data);
+
+// protected register → must send admin token
+export const registerUser = (data, token) =>
+  axios.post(`${API}/register`, data, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
